a97ef705886153da31be5ba34a1c7c6f6e85f0d8
Laura Melo <lamelob99@hotmail.com>
2022-04-23 17:14:08 -0400

    April 23, 2022 5:14 PM

---
+++ portfolio2/ExerciseScreen.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import {StyleSheet, Text, View, Button} from 'react-native';
+
+export default function ExerciseScreen() {
+    return(
+        <View style={StyleSheet.container}>
+            <Text>Adding Exercises here!</Text>
+            <StatusBar style="auto"/>
+            <Button title="Back to Home"
+            onPress={() =>
+                this.props.navigation.navigate('Home')
+            }
+            />
+
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
