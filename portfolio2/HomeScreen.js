d08edca946944db383dd8b726666f4bcd6711c58
Laura Melo <lamelob99@hotmail.com>
2022-04-24 15:10:06 -0400

    April 24, 2022 3:10 PM

--- portfolio2/HomeScreen.js
+++ portfolio2/HomeScreen.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button } from 'react-native';
 
 class HomeScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>You have (undefined) friends.</Text>
+        <Text>You have (undefined) exercise.</Text>
+        <Button
+          title="Add some exercise"
+          onPress={() =>
+            this.props.navigation.navigate('Exercise')
+          }
+        />
       </View>
     );
   }
