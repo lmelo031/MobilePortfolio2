4518ce000fc03845e9c67bbaf149a3448f2e9660
Laura Melo <lamelob99@hotmail.com>
2022-04-24 15:09:10 -0400

    April 24, 2022 3:09 PM

--- portfolio2/App.js
+++ portfolio2/App.js
@@ -1,12 +1,28 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './HomeScreen';
+import ExerciseScreen from './ExerciseScreen';
 
+const Stack = createStackNavigator();
 class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Welcome!</Text>
-      </View>
+      <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+          />
+      <Stack.Screen
+            name="Exercise"
+            component={ExerciseScreen}
+          />
+
+      </Stack.Navigator>
+    </NavigationContainer>
     );
   }
 }
