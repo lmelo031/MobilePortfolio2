25ed3e275010fc8c85e9535becbba064ab945b39
Laura Melo <lamelob99@hotmail.com>
2022-04-24 18:19:14 -0400

    April 24, 2022 6:19 PM

--- portfolio2/ExerciseScreen.js
+++ portfolio2/ExerciseScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View,Button } from 'react-native';
 import HomeScreen from './HomeScreen';
+
 class ExerciseScreen extends React.Component {
   render() {
     return (
