fb6653ad8a93ae00ffd656bf04df4d0a8e01c620
Laura Melo <lamelob99@hotmail.com>
2022-04-24 15:15:55 -0400

    April 24, 2022 3:15 PM

--- portfolio2/HomeScreen.js
+++ portfolio2/HomeScreen.js
@@ -17,7 +17,14 @@ class HomeScreen extends React.Component {
     );
   }
 }
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
 
-// ...
 
 export default HomeScreen;
