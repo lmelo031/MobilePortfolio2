de676b4c02af8698dc1002741295da6ce20ea234
Laura Melo <lamelob99@hotmail.com>
2022-04-24 13:41:24 -0400

    April 24, 2022 1:41 PM

--- portfolio2/StartScreen.js
+++ portfolio2/StartScreen.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import {Stylesheet, Text, View, Button} from 'react-native';
 
-export default function HomeScreen () {
+export default function StartScreen () {
     return (
         <View style={Stylesheet.container}>
             <Text>Displayed(undefined) exercises!</Text>
             <StatusBar style="auto"/>
             <Button title="Add some exercises"
             onPress={() =>
-                this.props.navigation.navigate('exercises')
+                this.props.navigation.navigate('Exercise')
             }
         />
         </View>
